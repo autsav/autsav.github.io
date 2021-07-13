2a2de6472a69b085a4b2587e43edc468f4d1ce67
autsav <autsav73@gmail.com>
2021-07-14 02:21:37 +0545

    token-3

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,19 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
-      options: {},
-    },
+   // In your gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-source-github-api`,
+    options: {
+      token: "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",
+      variables: {
+        q: "author:ldd state:closed type:pr sort:comments",
+        nFirst: 2
+      }
+    }
+  }
+],
     googleAnalyticsTrackingId && {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
