26579859d78a88231c4a50b660c9cf0f86244c75
autsav <autsav73@gmail.com>
2021-07-16 17:14:57 +0545

    repository error fixed 7

--- src/pages/githubprojects.js
+++ src/pages/githubprojects.js
@@ -17,32 +17,65 @@
 // export default IndexPage
 
 import * as React from 'react'
-// import { title } from './githubprojects.module.css'
-// import { loader } from './githubprojects.module.css'
+import  { Component } from 'react';
+
+import fetch from 'node-fetch'
+import ReactDOM from 'react-dom'
 
 
-const  IndexPage= () => <div >
-                    <h2> List of all the repository </h2>
-                    <ul id="repos"></ul>
-                    
-                      </div>
+class myComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://api.github.com/users/autsav/repos")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
 
-fetch("https://api.github.com/users/autsav/repos")
-.then(response => response.json())
-.then(function(data){
-  data.forEach(element => {
-    console.log(element.name)
-    let ul =  document.getElementById('repos');
 
-    let li = document.createElement('li');
 
-    ul.appendChild(li);
-    li.innerHTML= element.name;
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return ([
+        <h3>All my repository lists are:</h3>,
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+          ]);
+    }
+  }
+}
 
-    
-  });
- 
- 
-})
-
-export default IndexPage
+export default myComponent;
